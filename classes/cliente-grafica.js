85b1437ce399b724bc835ca04746f96b14a1ea3f
julio cesar calderon galindo <julcescalgalty@gmail.com>
2021-10-22 16:12:35 -0500

    subida de grafica

--- classes/cliente-grafica.js
+++ classes/cliente-grafica.js
@@ -1,18 +1,31 @@
 
-
+const { request, response } = require('express');
+const Cliente = require('../models/cliente');
 
 class ClienteGrafica{
 
-    datos = [10,20,30,40,50,60,70,80,90,10,11,12]
-    label = [0,1,2,3,4,5,6,7,8,9,10,11,12]
+    
+    
+    datos = [0,0,0,0,0,0,0,0,0,0,0,0]
+    meses = [1,2,3,4,5,6,7,8,9,10,11,12]
     
     constructor(){
-        
     }
 
 
     getClienteGrafica(){
         return [{data:this.datos, label: 'Clientes'}]
+    }
+    incrementarValor(me, valor){
+        for(let i in this.meses){
+            if (this.meses[i]===me) {
+                this.datos[i] += valor;
+            }
+        }
+        return this.getClienteGrafica();
+    }
+    getCliente(){
+        
     }
 }
 
--- routes/graficas.js
+++ routes/graficas.js
@@ -1,13 +1,14 @@
 const { response, Router } = require("express");
+const Cliente = require('../models/cliente')
 const ClienteGrafica = require("../classes/cliente-grafica");
 
 const router = Router();
 const graficalineal = new ClienteGrafica();
 router.get('/', async(req= request, res=response)=>{
-    const data = graficalineal.getClienteGrafica();
-    res.json({
-        data
-    })
+    const fecha = new Date();
+    const ano = fecha.getFullYear();
+    const data = await graficalineal.getClienteGrafica();
+    console.log(data);
 })
 
 
