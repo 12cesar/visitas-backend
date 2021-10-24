263c56bec4ab1131723f79afb99466462c2182fd
julio cesar calderon galindo <julcescalgalty@gmail.com>
2021-10-24 17:16:17 -0500

    x-2

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -90,7 +90,7 @@ const esMensajeIdValido = async(id)=>{
     } 
 }
 const esConduccionIdValido = async(id)=>{
-    const conduccion = await Conduccion.findOne({id});
+    const conduccion = await Conduccion.findById({id});
     if (!conduccion) {
         throw new Error(`El id ${id} no existe en la base de datos`);
     } 
